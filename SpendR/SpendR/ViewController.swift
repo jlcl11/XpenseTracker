c8da5da99c22a957d696f9dcae878c1dd71c49d8
José Luis Corral <>
2023-10-29 17:12:16 +0100

    App creation

---
+++ SpendR/SpendR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpendR
+//
+//  Created by José Luis Corral on 29/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
