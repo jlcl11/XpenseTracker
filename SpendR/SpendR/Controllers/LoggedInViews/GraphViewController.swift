7b45b765deb21c09b16ca64532adc46c04d6cc38
José Luis Corral <>
2023-11-17 13:48:51 +0100

    Changed chart type into a barchart

--- SpendR/SpendR/Controllers/LoggedInViews/GraphViewController.swift
+++ SpendR/SpendR/Controllers/LoggedInViews/GraphViewController.swift
@@ -5,27 +5,27 @@ class GraphViewController: UIViewController, ChartViewDelegate {
 
     @IBOutlet weak var graphView: UIView!
 
-    var lineChart = LineChartView()
+    var barChart = BarChartView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        lineChart.delegate = self
-        lineChart.setScaleEnabled(true) // Permitir hacer zoom en el gráfico
-        lineChart.extraBottomOffset = 10.0 // Agregar espacio entre el eje X y la leyenda
+        barChart.delegate = self
+        barChart.setScaleEnabled(true) // Permitir hacer zoom en el gráfico
+        barChart.extraBottomOffset = 10.0 // Agregar espacio entre el eje X y la leyenda
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
-        lineChart.frame = CGRect(x: 0, y: 0, width: graphView.frame.size.width, height: graphView.frame.size.height)
-        graphView.addSubview(lineChart)
+        barChart.frame = CGRect(x: 0, y: 0, width: graphView.frame.size.width, height: graphView.frame.size.height)
+        graphView.addSubview(barChart)
 
         guard let movements = UserManager.shared.getCurrentUser()?.movements else {
             return
         }
 
-        var entries = [ChartDataEntry]()
+        var entries = [BarChartDataEntry]()
 
         for (index, movement) in movements.enumerated() {
             var yValue = movement.properties.amount ?? 0.0
@@ -37,54 +37,39 @@ class GraphViewController: UIViewController, ChartViewDelegate {
 
             // Utilizar la fecha como valor en el eje x
             if let date = movement.properties.date {
-                entries.append(ChartDataEntry(x: date.timeIntervalSince1970, y: yValue))
+                entries.append(BarChartDataEntry(x: Double(index), y: yValue))
             }
         }
 
         // Ordenar las entradas por fecha ascendente
         entries.sort { $0.x < $1.x }
 
-        let set = LineChartDataSet(entries: entries, label: "Amount")
+        let set = BarChartDataSet(entries: entries, label: "Amount")
         set.colors = gradientColors(numColors: 5) // Utilizar 5 colores degradados de rojo a verde
         set.valueTextColor = .label
 
-        let data = LineChartData(dataSet: set)
+        let data = BarChartData(dataSet: set)
 
         // Configurar el formato de las fechas en el eje x
-        let xAxis = lineChart.xAxis
-        xAxis.valueFormatter = DateValueFormatter()
+        let xAxis = barChart.xAxis
+        xAxis.valueFormatter = IndexAxisValueFormatter(values: entries.map { String($0.x) }) // Mostrar índices en lugar de fechas
         xAxis.labelPosition = .bottom // Posicionar las etiquetas en la parte inferior
         xAxis.drawLabelsEnabled = true
 
         // Configurar el eje Y
-        let leftAxis = lineChart.leftAxis
+        let leftAxis = barChart.leftAxis
         leftAxis.labelPosition = .outsideChart // Posicionar las etiquetas a la izquierda
         leftAxis.drawLabelsEnabled = true
 
         // Configurar la leyenda
-        let legend = lineChart.legend
+        let legend = barChart.legend
         legend.textColor = .label
         legend.form = .square
         legend.formSize = 12.0
         legend.formToTextSpace = 5.0
         legend.horizontalAlignment = .left
 
-        lineChart.data = data
-    }
-
-    // Clase para formatear las fechas en el eje x
-    class DateValueFormatter: NSObject, AxisValueFormatter {
-        private let dateFormatter = DateFormatter()
-
-        override init() {
-            super.init()
-            dateFormatter.dateFormat = "yyyy"
-        }
-
-        func stringForValue(_ value: Double, axis: AxisBase?) -> String {
-            let date = Date(timeIntervalSince1970: value)
-            return dateFormatter.string(from: date)
-        }
+        barChart.data = data
     }
 
     // Función para generar colores degradados de rojo a verde
@@ -103,5 +88,13 @@ class GraphViewController: UIViewController, ChartViewDelegate {
         }
 
         return colors
+    }
+
+    // Método del delegado para manejar la selección de barras
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        if let barEntry = entry as? BarChartDataEntry {
+            let index = Int(barEntry.x)
+            print("Selected bar at index: \(index)")
+        }
     }
 }
