4c62fa65bde7337ce02325895c9f95e527e3e6e2
jlcl11 <92518378+jlcl11@users.noreply.github.com>
2023-12-16 13:25:49 +0100

    Redesigning screens for different resolutions

--- SpendR/SpendR/Controllers/InitialViews/LoginViewController.swift
+++ SpendR/SpendR/Controllers/InitialViews/LoginViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
